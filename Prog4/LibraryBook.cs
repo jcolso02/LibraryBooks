410a53b2562e88d02edda995b6069f85e306983d
jcols <jcols@DESKTOP-FR1KESF>
2023-04-08 18:34:51 -0400

    Add project files.

---
+++ Prog4/LibraryBook.cs
@@ -0,0 +1,81 @@
+﻿// S5034
+// CIS 199-50
+// Program 4
+// December 4, 2021
+
+// This file is to create LibraryBooks to use for storing thueir information in the correct place,
+// determining if they are checked out or not,
+// and returning them to the shelf.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    public class LibraryBook
+    {
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+        public string BookPublisher { get; set; }
+        public int yearOfPublication;
+        public int YearOfPublication
+        {
+            get
+            {
+                return yearOfPublication;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    yearOfPublication = value;
+                }
+                else
+                {
+                    yearOfPublication = 2019;
+                }
+            }
+        }
+        public string ISBN { get; set; }
+
+        public bool checkedOut { get; set; }
+
+        public LibraryBook(string Title, string Author, string Publisher, int Year, string CallNumber, bool CheckedOut)
+        {
+            BookTitle = Title;
+            BookAuthor = Author;
+            BookPublisher = Publisher;
+            YearOfPublication = Year;
+            ISBN = CallNumber;
+            checkedOut = CheckedOut;
+        }
+
+        public void CheckedOut()
+        {
+            checkedOut = true;
+        }
+
+        public void ReturnToShelf()
+        {
+            checkedOut = false;
+        }
+
+        public bool IsCheckedOut()
+        {
+            return checkedOut;
+        }
+
+        public override string ToString()
+        {
+            string output = $"Title: {BookTitle}{Environment.NewLine}" +
+                $" Author: {BookAuthor}{Environment.NewLine}" +
+                $"Publisher : {BookPublisher}{Environment.NewLine}" +
+                $"Year: {YearOfPublication}{Environment.NewLine}" +
+                $"Call Number: {ISBN}{Environment.NewLine}";
+            return output;
+        }
+    }
+}
---
+++ Prog4/Program.cs
@@ -0,0 +1,92 @@
+﻿// S5034
+// CIS 199-50
+// Program 4
+// December 4, 2021
+
+// This file is used to print the books and change their contents.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array of Library Books
+            LibraryBook[] books = {
+
+                new LibraryBook("Harry Potter and the Sorcerer's Stone",
+                "J. K. Rowling",
+                "Bloomsburry (UK)",
+                1997,
+                "9780590353403",
+                false),
+
+                new LibraryBook("Hunger Games",
+                "Suzanne Collins",
+                "Scholastic Press",
+                2008,
+                "9780590353402",
+                false),
+
+                new LibraryBook("Dante's Inferno",
+                "Dante Alighieri",
+                "",
+                1826,
+                "9780374176747",
+                false),
+
+                new LibraryBook("Make Your Bed",
+                "William H. McRaven",
+                "Grand Central Publishing",
+                2017,
+                "9781455570249",
+                false),
+
+                new LibraryBook("Series of Unfortunate Events",
+                "Lemony Snicket",
+                "HarperCollins",
+                1999,
+                "97805903536847",
+                false) };
+
+            PrintBooks(books); // First Print, No Change
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            books[0].CheckedOut();
+            books[1].CheckedOut();
+
+            books[2].BookPublisher = "Publisher 1";
+            books[3].ISBN = "930485739385";
+            books[4].BookPublisher = "Publisher 2";
+
+            PrintBooks(books); // Second Print, after 2 are checked out, 2 publishers are changed, and an ISBN is changed
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            books[0].ReturnToShelf();
+            books[1].ReturnToShelf();
+
+            PrintBooks(books); // Third Print, after 2 checked out books are retuned to the shelf
+
+            Console.ReadKey(); // Allowing user to see printed books
+        }
+
+        public static void PrintBooks(LibraryBook[] libraryBooks) // Method used to print books
+        {
+            // Loop to print books
+            for(int i = 0; i < libraryBooks.Length; i++)
+            {
+                Console.WriteLine(libraryBooks[i].ToString());
+            }
+        }
+    }
+}
